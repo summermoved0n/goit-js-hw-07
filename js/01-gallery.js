026b01600bca5cd3ab0c3df6f48753faae6e8d0c
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-10-13 12:59:41 -0400

    add photo

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+const items = galleryItems.map(({ preview, description }) => {
+  return `<li class='item'><img class="gallery__item" src ="${preview}" alt='${description}' width='300'></li>`;
+}).join('');
+list.insertAdjacentHTML("afterbegin", items);
