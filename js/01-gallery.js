895a9fc361fa6927d671d121daca8e018d4b3432
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-10-14 19:42:54 -0400

    task 1 is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,19 @@ console.log(galleryItems);
 
 const list = document.querySelector(".gallery");
 
-const items = galleryItems.map(({ preview, description }) => {
-  return `<li class='item'><img class="gallery__item" src ="${preview}" alt='${description}' width='300'></li>`;
-}).join('');
-list.insertAdjacentHTML("afterbegin", items);
+const markup = galleryItems
+  .map(({ preview, description, original }) => {
+    return `<li class='gallery__item'><img class="gallery__image" src ="${preview}" alt='${description}' data-original='${original}'></li>`;
+  })
+  .join("");
+list.insertAdjacentHTML("afterbegin", markup);
+
+list.addEventListener("click", getBasicLightbox);
+
+function getBasicLightbox(event) {
+  basicLightbox
+    .create(
+      `<img src=${event.target.dataset.original}>`
+    )
+    .show();
+}
